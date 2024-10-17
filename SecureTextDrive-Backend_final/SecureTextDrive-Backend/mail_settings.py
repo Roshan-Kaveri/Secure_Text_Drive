cd901f8833b08c8889e964c0bdb813ca65e0c0c1
Roshan-Kaveri <161740353+Roshan-Kaveri@users.noreply.github.com>
2024-10-17 21:38:05 +0530

    Update mail_settings.py

--- SecureTextDrive-Backend_final/SecureTextDrive-Backend/mail_settings.py
+++ SecureTextDrive-Backend_final/SecureTextDrive-Backend/mail_settings.py
@@ -2,7 +2,7 @@
 
 MAIL_SERVER = 'smtp.hostinger.com'
 MAIL_PORT = 465
-MAIL_USERNAME = 'support@hmmbo.com'
-MAIL_PASSWORD = 'R0$hankumar'
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
